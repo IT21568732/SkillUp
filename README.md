13bd17689c90e31f41dc297c74db7ed5faef0617
Tharusha Ravishka Ranasinghe <tharusharavishka19@gmail.com>
2024-05-08 04:19:30 +0530

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SkillUp
 SkillUp is a Educational Platform for Online Learning developed using microservice architecture
+
+Docker is not used,pure node js
+
+port no:
+
+4000 - course
+5000 - learner
+6000 - order
+7000 - payment
--- backend/course_managment_ms/index.js
+++ backend/course_managment_ms/index.js
@@ -1,1 +1,15 @@
-test
+const express = require("express");
+const app = express();
+//tharusha
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://1234567:RNTlODZafCDL6Ybh@cluster0.utm68bw.mongodb.net/demo")
+
+app.get('/',(req,res)=>{
+    res.send("main endpoint");
+})
+
+const PORT = 4000;
+app.listen(PORT,()=>{
+    console.log(`Course_ms---Up and Running on port ${PORT}`)
+})
