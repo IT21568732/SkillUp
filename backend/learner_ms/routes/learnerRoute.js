7cd499ae69881907ce389a0bd756106a41f0bcfe
Sahan Nimantha <it21281532@my.sliit.lk>
2024-05-17 10:58:48 +0530

    4

--- backend/learner_ms/routes/learnerRoute.js
+++ backend/learner_ms/routes/learnerRoute.js
@@ -2,34 +2,55 @@
 // const router = express.Router();
 // const learnerController = require('../controllers/learnerController');
 
+// // Route to enroll a learner in a course
 // router.post('/enroll', learnerController.enrollCourse);
-// router.get('/enrolled-courses', learnerController.getEnrolledCourses);
-// router.put('/update-progress', learnerController.updateProgress);
+
+// // Route to get enrolled courses of a learner
+// router.post('/enrolled-courses', learnerController.getEnrolledCourses);
+
+// // Route to get details of an enrolled course by its ID
+// router.post('/enrolled-course-details', learnerController.getEnrolledCourseById);
+
+// // Route to get progress of a course
+// router.post('/course-progress', learnerController.getCourseProgress);
+
+// // Route to update progress of a course
+// router.put('/update-course-progress', learnerController.updateCourseProgress);
+
+// // Route to delete an enrolled course
+// router.delete('/delete-enrolled-course', learnerController.deleteEnrolledCourse);
 
 // module.exports = router;
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const learnerController = require('../controllers/learnerController');
+const learnerController = require("../controllers/learnerController");
 
 // Route to enroll a learner in a course
-router.post('/enroll', learnerController.enrollCourse);
+router.post("/enroll", learnerController.enrollCourse);
 
 // Route to get enrolled courses of a learner
-router.post('/enrolled-courses', learnerController.getEnrolledCourses);
+router.post("/enrolled-courses", learnerController.getEnrolledCourses);
 
 // Route to get details of an enrolled course by its ID
-router.post('/enrolled-course-details', learnerController.getEnrolledCourseById);
+router.post(
+  "/enrolled-course-details",
+  learnerController.getEnrolledCourseById
+);
 
 // Route to get progress of a course
-router.post('/course-progress', learnerController.getCourseProgress);
+router.post("/course-progress", learnerController.getCourseProgress);
 
 // Route to update progress of a course
-router.put('/update-course-progress', learnerController.updateCourseProgress);
+router.put("/update-course-progress", learnerController.updateCourseProgress);
 
 // Route to delete an enrolled course
-router.delete('/delete-enrolled-course', learnerController.deleteEnrolledCourse);
+router.delete(
+  "/delete-enrolled-course",
+  learnerController.deleteEnrolledCourse
+);
+
+// Route to unenroll a course
+router.delete("/unenroll-course", learnerController.unenrollCourse); // Add this line
 
 module.exports = router;
-
-
--- backend/learner_ms/services/LearnerService.js
+++ backend/learner_ms/services/LearnerService.js
@@ -1,176 +1,371 @@
+// const Learner = require("../model/learnerModel");
+// const axios = require("axios");
+
+// class LearnerService {
+//     // Enroll a learner in a course
+//     async enrollCourse(payload, res) {
+//         try {
+//             const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+
+//             if (!learner) {
+//                 const newLearner = await Learner.create(payload);
+//                 return {
+//                     success: true,
+//                     data: newLearner,
+//                     message: "Learner created and course enrolled successfully",
+//                 };
+//             }
+
+//             const newCourseIds = payload.enrolledCourses.map(course => course.courseId);
+//             const existingCourseIds = learner.enrolledCourses.map(course => course.courseId);
+//             const mergedCourseIds = [...existingCourseIds, ...newCourseIds];
+//             const uniqueCourseIds = [...new Set(mergedCourseIds)];
+
+//             learner.enrolledCourses = uniqueCourseIds.map(courseId => ({ courseId }));
+
+//             const updatedLearner = await learner.save();
+
+//             return {
+//                 success: true,
+//                 data: updatedLearner,
+//                 message: "Course enrolled successfully",
+//             };
+//         } catch (err) {
+//             return {
+//                 success: false,
+//                 message: err.message,
+//             };
+//         }
+//     }
+
+//     // Get enrolled courses of a learner
+//     async getEnrolledCourses(payload, token) {
+//         try {
+//             const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+//             if (!learner) {
+//                 return {
+//                     message: "No courses enrolled",
+//                 };
+//             }
+
+//             const enrolledCourseIds = learner.enrolledCourses.map(course => course.courseId);
+//             const courseDetailsPromises = enrolledCourseIds.map(async courseId => {
+//                 const courseDetailsResponse = await axios.get(`http://localhost:8001/course/course_by_id/${courseId}`, {
+//                     headers: {
+//                         //Authorization: `Bearer ${token}`,
+//                     },
+//                 });
+//                 return courseDetailsResponse.data;
+//             });
+
+//             const courseDetails = await Promise.all(courseDetailsPromises);
+
+//             const response = {
+//                 courseDetails: courseDetails,
+//             };
+
+//             return {
+//                 success: true,
+//                 data: response,
+//                 message: "Courses fetched successfully",
+//             };
+//         } catch (err) {
+//             return {
+//                 success: false,
+//                 message: err.message,
+//             };
+//         }
+//     }
+
+//     // Get progress of a course
+//     async getCourseProgress(payload) {
+//         try {
+//             const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+//             if (!learner) {
+//                 return {
+//                     message: "No courses enrolled",
+//                 };
+//             }
+
+//             const course = learner.enrolledCourses.find(course => course.courseId === payload.courseId);
+//             if (!course) {
+//                 return {
+//                     message: "Course not enrolled",
+//                 };
+//             }
+
+//             const courseProgress = {
+//                 progress: course.progress,
+//             };
+
+//             return {
+//                 data: courseProgress,
+//                 message: "Course progress fetched successfully",
+//             };
+//         } catch (err) {
+//             return {
+//                 success: false,
+//                 message: err.message,
+//             };
+//         }
+//     }
+
+//     // Update progress of a course
+//     async updateCourseProgress(payload) {
+//         try {
+//             const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+//             if (!learner) {
+//                 return {
+//                     message: "No courses enrolled",
+//                 };
+//             }
+
+//             const courseIndex = learner.enrolledCourses.findIndex(course => course.courseId === payload.courseId);
+//             if (courseIndex === -1) {
+//                 return {
+//                     message: "Course not enrolled",
+//                 };
+//             }
+
+//             learner.enrolledCourses[courseIndex].progress = payload.progress;
+//             await learner.save();
+
+//             return {
+//                 message: "Course progress updated successfully",
+//             };
+//         } catch (err) {
+//             return {
+//                 success: false,
+//                 message: err.message,
+//             };
+//         }
+//     }
+
+//     // Delete enrolled course
+//     async deleteEnrolledCourse(payload) {
+//         try {
+//             const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+//             if (!learner) {
+//                 return {
+//                     message: "No courses enrolled",
+//                 };
+//             }
+
+//             const courseIndex = learner.enrolledCourses.findIndex(course => course.courseId === payload.courseId);
+//             if (courseIndex === -1) {
+//                 return {
+//                     message: "Course not enrolled",
+//                 };
+//             }
+
+//             learner.enrolledCourses.splice(courseIndex, 1);
+//             await learner.save();
+
+//             return {
+//                 message: "Course unenrolled successfully",
+//             };
+//         } catch (err) {
+//             return {
+//                 success: false,
+//                 message: err.message,
+//             };
+//         }
+//     }
+// }
+
+// module.exports = LearnerService;
+
 const Learner = require("../model/learnerModel");
 const axios = require("axios");
 
 class LearnerService {
-    // Enroll a learner in a course
-    async enrollCourse(payload, res) {
-        try {
-            const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+  // Enroll a learner in a course
+  async enrollCourse(payload, res) {
+    try {
+      const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
 
-            if (!learner) {
-                const newLearner = await Learner.create(payload);
-                return {
-                    success: true,
-                    data: newLearner,
-                    message: "Learner created and course enrolled successfully",
-                };
-            }
+      if (!learner) {
+        const newLearner = await Learner.create(payload);
+        return {
+          success: true,
+          data: newLearner,
+          message: "Learner created and course enrolled successfully",
+        };
+      }
 
-            const newCourseIds = payload.enrolledCourses.map(course => course.courseId);
-            const existingCourseIds = learner.enrolledCourses.map(course => course.courseId);
-            const mergedCourseIds = [...existingCourseIds, ...newCourseIds];
-            const uniqueCourseIds = [...new Set(mergedCourseIds)];
+      const newCourseIds = payload.enrolledCourses.map(
+        (course) => course.courseId
+      );
+      const existingCourseIds = learner.enrolledCourses.map(
+        (course) => course.courseId
+      );
+      const mergedCourseIds = [...existingCourseIds, ...newCourseIds];
+      const uniqueCourseIds = [...new Set(mergedCourseIds)];
 
-            learner.enrolledCourses = uniqueCourseIds.map(courseId => ({ courseId }));
+      learner.enrolledCourses = uniqueCourseIds.map((courseId) => ({
+        courseId,
+      }));
 
-            const updatedLearner = await learner.save();
+      const updatedLearner = await learner.save();
 
-            return {
-                success: true,
-                data: updatedLearner,
-                message: "Course enrolled successfully",
-            };
-        } catch (err) {
-            return {
-                success: false,
-                message: err.message,
-            };
-        }
+      return {
+        success: true,
+        data: updatedLearner,
+        message: "Course enrolled successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
     }
+  }
 
-    // Get enrolled courses of a learner
-    async getEnrolledCourses(payload, token) {
-        try {
-            const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
-            if (!learner) {
-                return {
-                    message: "No courses enrolled",
-                };
-            }
+  // Get enrolled courses of a learner
+  async getEnrolledCourses(payload, token) {
+    try {
+      const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+      if (!learner) {
+        return {
+          message: "No courses enrolled",
+        };
+      }
 
-            const enrolledCourseIds = learner.enrolledCourses.map(course => course.courseId);
-            const courseDetailsPromises = enrolledCourseIds.map(async courseId => {
-                const courseDetailsResponse = await axios.get(`http://localhost:8001/course/course_by_id/${courseId}`, {
-                    headers: {
-                        //Authorization: `Bearer ${token}`,
-                    },
-                });
-                return courseDetailsResponse.data;
-            });
+      const enrolledCourseIds = learner.enrolledCourses.map(
+        (course) => course.courseId
+      );
+      const courseDetailsPromises = enrolledCourseIds.map(async (courseId) => {
+        const courseDetailsResponse = await axios.get(
+          `http://localhost:8001/course/course_by_id/${courseId}`,
+          {
+            headers: {
+              //Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        return courseDetailsResponse.data;
+      });
 
-            const courseDetails = await Promise.all(courseDetailsPromises);
+      const courseDetails = await Promise.all(courseDetailsPromises);
 
-            const response = {
-                courseDetails: courseDetails,
-            };
+      const response = {
+        courseDetails: courseDetails,
+      };
 
-            return {
-                success: true,
-                data: response,
-                message: "Courses fetched successfully",
-            };
-        } catch (err) {
-            return {
-                success: false,
-                message: err.message,
-            };
-        }
+      return {
+        success: true,
+        data: response,
+        message: "Courses fetched successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
     }
+  }
 
-    // Get progress of a course
-    async getCourseProgress(payload) {
-        try {
-            const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
-            if (!learner) {
-                return {
-                    message: "No courses enrolled",
-                };
-            }
+  // Get progress of a course
+  async getCourseProgress(payload) {
+    try {
+      const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+      if (!learner) {
+        return {
+          message: "No courses enrolled",
+        };
+      }
 
-            const course = learner.enrolledCourses.find(course => course.courseId === payload.courseId);
-            if (!course) {
-                return {
-                    message: "Course not enrolled",
-                };
-            }
+      const course = learner.enrolledCourses.find(
+        (course) => course.courseId === payload.courseId
+      );
+      if (!course) {
+        return {
+          message: "Course not enrolled",
+        };
+      }
 
-            const courseProgress = {
-                progress: course.progress,
-            };
+      const courseProgress = {
+        progress: course.progress,
+      };
 
-            return {
-                data: courseProgress,
-                message: "Course progress fetched successfully",
-            };
-        } catch (err) {
-            return {
-                success: false,
-                message: err.message,
-            };
-        }
+      return {
+        data: courseProgress,
+        message: "Course progress fetched successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
     }
+  }
 
-    // Update progress of a course
-    async updateCourseProgress(payload) {
-        try {
-            const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
-            if (!learner) {
-                return {
-                    message: "No courses enrolled",
-                };
-            }
+  // Update progress of a course
+  async updateCourseProgress(payload) {
+    try {
+      const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
+      if (!learner) {
+        return {
+          message: "No courses enrolled",
+        };
+      }
 
-            const courseIndex = learner.enrolledCourses.findIndex(course => course.courseId === payload.courseId);
-            if (courseIndex === -1) {
-                return {
-                    message: "Course not enrolled",
-                };
-            }
+      const courseIndex = learner.enrolledCourses.findIndex(
+        (course) => course.courseId === payload.courseId
+      );
+      if (courseIndex === -1) {
+        return {
+          message: "Course not enrolled",
+        };
+      }
 
-            learner.enrolledCourses[courseIndex].progress = payload.progress;
-            await learner.save();
+      learner.enrolledCourses[courseIndex].progress = payload.progress;
+      await learner.save();
 
-            return {
-                message: "Course progress updated successfully",
-            };
-        } catch (err) {
-            return {
-                success: false,
-                message: err.message,
-            };
-        }
+      return {
+        message: "Course progress updated successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
     }
+  }
 
-    // Delete enrolled course
-    async deleteEnrolledCourse(payload) {
-        try {
-            const learner = await Learner.findOne({ Learner_ID: payload.Learner_ID });
-            if (!learner) {
-                return {
-                    message: "No courses enrolled",
-                };
-            }
+  // Delete enrolled course
+  async deleteEnrolledCourse(payload) {
+    try {
+      const { Learner_ID, courseId } = payload;
+      const learner = await Learner.findOne({ Learner_ID });
+      if (!learner) {
+        return {
+          message: "No courses enrolled",
+        };
+      }
 
-            const courseIndex = learner.enrolledCourses.findIndex(course => course.courseId === payload.courseId);
-            if (courseIndex === -1) {
-                return {
-                    message: "Course not enrolled",
-                };
-            }
+      const courseIndex = learner.enrolledCourses.findIndex(
+        (course) => course.courseId === courseId
+      );
+      if (courseIndex === -1) {
+        return {
+          message: "Course not enrolled",
+        };
+      }
 
-            learner.enrolledCourses.splice(courseIndex, 1);
-            await learner.save();
+      learner.enrolledCourses.splice(courseIndex, 1);
+      await learner.save();
 
-            return {
-                message: "Course unenrolled successfully",
-            };
-        } catch (err) {
-            return {
-                success: false,
-                message: err.message,
-            };
-        }
+      return {
+        message: "Course unenrolled successfully",
+      };
+    } catch (err) {
+      return {
+        success: false,
+        message: err.message,
+      };
     }
+  }
 }
 
 module.exports = LearnerService;
